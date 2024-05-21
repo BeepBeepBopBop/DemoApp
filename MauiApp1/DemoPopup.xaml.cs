38618038c9a53e27f67c63408971e7c9c9e97f1a
BeepBeepBopBop <evan.carrere@gmail.com>
2024-05-21 15:57:06 +0200

    update

---
+++ MauiApp1/DemoPopup.xaml.cs
@@ -0,0 +1,11 @@
+using DemoLibrary;
+
+namespace MauiApp1;
+
+public partial class DemoPopup : PopupView
+{
+	public DemoPopup()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DemoLibrary;
+using Mopups.Interfaces;
+using Mopups.Services;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var popup = new DemoPopup();
+
+            await DisplayPopup(popup);
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var popup = new LibDemoPopup();
+
+            await DisplayPopup(popup);
+
+        }
+
+        public async Task<PopupBase.PopupResult> DisplayPopup(PopupBase popup)
+        {
+            await MopupService.Instance.PushAsync(popup);
+            var result = await popup.PopupDismissedTask;
+            return result;
+        }
+    }
+
+}
